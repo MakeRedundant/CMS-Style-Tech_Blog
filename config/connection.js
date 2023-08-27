243876652dde0d5e9494ff567e995bb9c4a156fc
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-08-27 21:38:00 +1000

    added connection.json

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,22 @@
 const Sequelize = require('sequelize');
-// dotenv for environmental variables for user name, password,  db_name (to help store personal information)
-require('dotenv').config();
+require('dotenv').config(); //Loads environment variables from a .env file into the process.env object.
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
+  sequelize = new Sequelize(process.env.JAWSDB_URL); // The code sets up a connection to the database based on whether the process.env.JAWSDB_URL environment variable is present.
+// This variable is typically provided by hosting platforms like Heroku to establish a database connection.
+} else { //if process.env.JAWSDB_URL doesnt exists create a new instance
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
 }
 
 module.exports = sequelize;
