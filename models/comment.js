082f9a0bdbf9032255db42be6ee40544b7b61417
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-08-27 22:59:46 +1000

    defined comment model

---
+++ models/comment.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// the Comment model extends the sequelize model
+class Comment extends Model {}
+// Declares the class a class named comment and the class will inherit from the sequelize model class
+
+// Defines the comment
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        // Comments must be at least one character long
+        len: [1],
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+// Exports the model
+module.exports = Comment;
