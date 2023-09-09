c2d203fcd0ec0b9e1a5a74bf266120f5527cf74e
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-09-09 21:12:53 +1000

    added auth.js and helpers.js

--- utils/auth.js
+++ utils/auth.js
@@ -7,3 +7,11 @@ const withAuth = (req, res, next) => {
 };
 
 module.exports = withAuth;
+
+//in summary, this middleware ensures that a user is authenticated (i.e., has a valid user session) before allowing access to certain routes.
+// If not authenticated, it redirects the user to a login page, and if authenticated, it lets the request proceed to the protected route.
+
+//When any request is made to a route that uses withAuth
+//it checks if req.session.user.iq exist,
+//if it doesnt not exist (user not authenicated it redirects to the login route, so the user is not logged in and they need to log in)
+//if it does exist it calls next() which is a function that tells express to move on in the chain.
