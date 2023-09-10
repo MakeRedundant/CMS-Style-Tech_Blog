6ffb65ca1c5358d93e8286265f9771287b7c7a28
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-09-10 13:51:39 +1000

    fixes to controllers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
+// Home route that displays all posts and associated comments
 router.get('/', (req, res) => {
   console.log(req.session);
 
+  // Fetch all posts and their associated comments and authors
   Post.findAll({
     attributes: ['id', 'title', 'created_at', 'post_content'],
     include: [
@@ -23,8 +25,9 @@ router.get('/', (req, res) => {
     ],
   })
     .then((PostData) => {
+      // Serialize the data for rendering
       const posts = PostData.map((post) => post.get({ plain: true }));
-      res.render('./', {
+      res.render('homepage', {
         posts,
         loggedIn: req.session.loggedIn,
       });
@@ -35,6 +38,7 @@ router.get('/', (req, res) => {
     });
 });
 
+// Login route, redirects to home if already logged in
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
@@ -44,6 +48,7 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+// Signup route, redirects to home if already logged in
 router.get('/signup', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
@@ -53,7 +58,9 @@ router.get('/signup', (req, res) => {
   res.render('signup');
 });
 
+// Single post route that displays a specific post and its comments
 router.get('/post/:id', (req, res) => {
+  // Find a specific post by ID and include associated comments and authors
   Post.findOne({
     where: {
       id: req.params.id,
@@ -76,14 +83,15 @@ router.get('/post/:id', (req, res) => {
   })
     .then((PostData) => {
       if (!PostData) {
+        // If no post found, return a 404 response
         res.status(404).json({ message: 'No post found with this id' });
         return;
       }
 
-      // serialize the data
+      // Serialize the data for rendering
       const post = PostData.get({ plain: true });
 
-      // pass data to template
+      // Pass data to the template for rendering
       res.render('single-post', {
         post,
         loggedIn: req.session.loggedIn,
--- seeds/index.js
+++ seeds/index.js
@@ -5,19 +5,45 @@ const seedComments = require('./comment-seeds');
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-  await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
+  // Sync the database and handle success/error
+  await sequelize
+    .sync({ force: true })
+    .then(() => {
+      console.log('\n----- DATABASE SYNCED -----\n');
+    })
+    .catch((err) => {
+      console.error('\n----- DATABASE SYNC FAILED -----\n', err);
+    });
+  // Seed users and handle success/error
+  await seedUsers()
+    .then(() => {
+      console.log('\n----- USERS SEEDED -----\n');
+    })
+    .catch((err) => {
+      console.error('\n----- USERS SEEDING FAILED -----\n', err);
+    });
+  // Seed posts and handle success/error
+  await seedPosts()
+    .then(() => {
+      console.log('\n----- POSTS SEEDED -----\n');
+    })
+    .catch((err) => {
+      console.error('\n----- POSTS SEEDING FAILED -----\n', err);
+    });
+  // Seed comments and handle success/error
+  await seedComments()
+    .then(() => {
+      console.log('\n----- COMMENTS SEEDED -----\n');
+    })
+    .catch((err) => {
+      console.error('\n----- COMMENTS SEEDING FAILED -----\n', err);
+    });
 
-  await seedUsers();
-  console.log('\n----- USERS SEEDED -----\n');
-
-  await seedPosts();
-  console.log('\n----- POSTS SEEDED -----\n');
-
-  await seedComments();
-  console.log('\n----- COMMENTS SEEDED -----\n');
-
+  // Exit the process when all seeding is done
   process.exit(0);
 };
 
+// Call the seedAll function to start the seeding process
 seedAll();
+
+//Catch method in the promise chain to catch erros that occur at any point in the chain.
